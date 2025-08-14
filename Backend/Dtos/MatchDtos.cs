3f35a633a3808d1ea961c08746fe8cfbb6543a74
ravin00 <ravinbandara76@gmail.com>
2025-08-15 00:06:54 +0530

    Add MatchDto class for match data transfer

---
+++ Backend/Dtos/MatchDtos.cs
@@ -0,0 +1,9 @@
+namespace Backend.Dtos
+{
+    public class MatchDto
+    {
+        public int JobId { get; set; }
+        public int UserId { get; set; }
+        public string MatchPrecentage { get; set; }
+    }
+}
