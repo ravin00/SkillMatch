b2bdf193ba2d95583cc566afcfb774fdf16cd177
ravin00 <ravinbandara76@gmail.com>
2025-09-03 18:03:35 +0530

    Add Task model with properties for Id, Title, Status, and ProjectId

---
+++ Backend/Models/Task/Task.cs
@@ -0,0 +1,12 @@
+namespace Backend.Models.Task;
+
+public class Task
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Title { get; set; } = string.Empty;
+
+    public string Status { get; set; } = "todo";
+
+    public string ProjectId { get; set; } = string.Empty;
+
+}
